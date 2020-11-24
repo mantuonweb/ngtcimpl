2fb2e9f8178c117d7343b0afcf191bbd2cbe45b6
mantuonweb <mantuonweb@hotmail.com>
2020-11-24 19:46:00 +0530

    Update load-script.service.spec.ts

--- src/app/load-script.service.spec.ts
+++ src/app/load-script.service.spec.ts
@@ -1,16 +1,78 @@
-import { TestBed } from '@angular/core/testing';
-
+import { fakeAsync, flush, TestBed } from '@angular/core/testing';
 import { ScriptLoaderService } from './load-script.service';
-
 describe('LoadScriptService', () => {
   let service: ScriptLoaderService;
-
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ScriptLoaderService]
+    });
     service = TestBed.inject(ScriptLoaderService);
   });
 
   it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('Load Script Should Load Success No Script preloaded', () => {
+    let loadObj: any = {
+      onload() { },
+      onerror() { }
+    };
+    let getElementsByTagNameMock: any = [{
+      appendChild() { }
+    }];
+    spyOn(window.document, 'getElementsByTagName').and.returnValue(getElementsByTagNameMock);
+    spyOn(window.document, 'createElement').and.returnValue(loadObj);
+    service.load({
+      name: "google-signin-plateform",
+      src: 'https://apis.google.com/js/platform.js',
+      loaded: false
+    }).subscribe();
+    loadObj.onload();
+    expect(service).toBeTruthy();
+  });
+
+  it('Load Script Should Load Success Script preloaded', () => {
+    let loadObj: any = {
+      onload() { },
+      onerror() { }
+    };
+    let getElementsByTagNameMock: any = [{
+      appendChild() { }
+    }];
+    spyOn(window.document, 'getElementsByTagName').and.returnValue(getElementsByTagNameMock);
+    spyOn(window.document, 'createElement').and.returnValue(loadObj);
+    service.load({
+      name: "google-signin-plateform",
+      src: 'https://apis.google.com/js/platform.js',
+      loaded: false
+    }).subscribe(() => {
+      service.load({
+        name: "google-signin-plateform",
+        src: 'https://apis.google.com/js/platform.js',
+        loaded: false
+      }).subscribe();
+    });
+
+    loadObj.onload();
+    expect(service).toBeTruthy();
+  });
+  it('Load Script Should Load Error', () => {
+    let loadObj: any = {
+      onload() { },
+      onerror() { }
+    };
+    let getElementsByTagNameMock: any = [{
+      appendChild() { }
+    }];
+    spyOn(window.document, 'getElementsByTagName').and.returnValue(getElementsByTagNameMock);
+    spyOn(window.document, 'createElement').and.returnValue(loadObj);
+    service.load({
+      name: "google-signin-plateform",
+      src: 'https://apis.google.com/js/platform.js',
+      loaded: false
+    }).subscribe();
+    loadObj.onerror();
     expect(service).toBeTruthy();
   });
 });
