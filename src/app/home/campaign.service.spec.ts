24713108a833525a67fb63586bdeefdab9763e37
mantuonweb <mantuonweb@hotmail.com>
2020-11-28 20:12:05 +0530

    test cases

--- src/app/home/campaign.service.spec.ts
+++ src/app/home/campaign.service.spec.ts
@@ -1,16 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 import { CampaignService } from './campaign.service';
 
 describe('CampaignService', () => {
   let service: CampaignService;
+  const mockHttp = {
+    get:()=>{
 
+    }
+  };
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers:[
+        { provide: HttpClient, useValue: mockHttp },
+        CampaignService
+      ]
+    });
     service = TestBed.inject(CampaignService);
   });
 
-  it('should be created', () => {
+  it('should be created 1', () => {
     expect(service).toBeTruthy();
+  });
+  it('should be created 1', () => {
+    let serviceInc:any = service;
+    spyOn(serviceInc.http,'get');
+    service.getCampaigns();
+    let url = environment.serverURL
+    expect(serviceInc.http.get).toHaveBeenCalledWith(url+"/"+"campaigns");
   });
 });
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,16 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { SharedModule } from '../shared/shared.module';
+import { CampaignService } from './campaign.service';
 
 import { HomeComponent } from './home.component';
-
+var campData:any[] = [{ "campaign_id": "My campaign ID", "campaign_name": "February 2020", "start_date": "2020-02-01", "end_date": "2020-02-29" }, { "campaign_id": "My campaign ID2", "campaign_name": "March 2020", "start_date": "2020-03-01", "end_date": "2020-03-31" }];
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  const mockCampaignService = {
+    getCampaigns:()=>{
+      return of(campData);
+    }
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
-    })
-    .compileComponents();
+      imports:[SharedModule,RouterTestingModule],
+      declarations: [HomeComponent],
+      providers:[
+        { provide: CampaignService, useValue: mockCampaignService }
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -21,5 +33,14 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should Load Data', (done) => {
+    component.ngOnInit();
+    component.onGridReady({});
+    fixture.detectChanges();
+    fixture.whenStable().then(()=>{
+      done();
+      expect(component.rowData.length).toBe(2);
+    });
   });
 });
